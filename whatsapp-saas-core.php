90266df6d4345e3f6d9fac739a0fbdc15d673b52
Alex Alves <nasalexalves@gmail.com>
2026-07-21 15:40:13 -0300

    up-version

--- whatsapp-saas-core.php
+++ whatsapp-saas-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WhatsApp SaaS Core
  * Description: Transform WordPress into a multi-tenant WhatsApp SaaS using Meta Official API.
- * Version: 0.5.5
+ * Version: 0.5.6
  * Author:            Equipe do Produto
  * Text Domain:       whatsapp-saas-core
  * Requires at least: 6.0
